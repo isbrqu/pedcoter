5d08f5b576fa673829aa33787f1f5d4cf47e102c
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-09-25 16:17:13 -0300

    incorpora el módulo logging

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from moodleapi import Moodle
 from csv import DictWriter
 from resource import Resource
 import time
+import logging
 
 def read_ref(filename):
     with open(filename, 'r') as file:
@@ -17,6 +18,8 @@ def write_ref(filename, ref):
 api = Moodle(url=config.url, token=config.moodle_token)
 course = api.courses[1]
 bot = TelegramBot(config.telegram_token, config.chat_id)
+logging.basicConfig(filename='output.log', level=logging.INFO)
+logging.info('Started')
 
 filename = 'ref.txt'
 
@@ -24,16 +27,18 @@ new_timemodified = 0
 i = 0
 
 while True:
-    print('consulta', i)
+    logging.info('query:' + str(i))
     response = api.resources_by_course(course)
     ref_timemodified = read_ref(filename)
     for resource in response['resources']:
         resource = Resource(resource)
         if resource.timemodified > ref_timemodified:
+            logging.info('send resource' + resource.name)
             bot.send_resource(resource)
             time.sleep(3)
             if resource.timemodified > new_timemodified:
                 new_timemodified = resource.timemodified
+    logging.info('timeout...')
     time.sleep(config.timeout)
     if new_timemodified > ref_timemodified:
         write_ref(filename, new_timemodified)
