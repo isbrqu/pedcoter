ce9726d197e44b6536051ed958d0e02c092b651e
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-09-25 15:57:47 -0300

    captura el tiempo de referencia en un archivo

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ from csv import DictWriter
 from resource import Resource
 import time
 
+def read_ref(filename):
+    with open(filename, 'r') as file:
+        ref = int(file.read())
+    return ref
+
+def write_ref(filename, ref):
+    with open(filename, 'w') as file:
+        file.write(str(ref))
+
 api = Moodle(url=config.url, token=config.moodle_token)
-course = api.courses[0]
+course = api.courses[1]
 bot = TelegramBot(config.telegram_token, config.chat_id)
 
-ref_timemodified = 0
+filename = 'ref.txt'
+
 new_timemodified = 0
 i = 0
 
 while True:
     print('consulta', i)
     response = api.resources_by_course(course)
+    ref_timemodified = read_ref(filename)
     for resource in response['resources']:
         resource = Resource(resource)
         if resource.timemodified > ref_timemodified:
@@ -25,6 +36,6 @@ while True:
                 new_timemodified = resource.timemodified
     time.sleep(config.timeout)
     if new_timemodified > ref_timemodified:
-        ref_timemodified = new_timemodified
+        write_ref(filename, new_timemodified)
     i += 1
 
